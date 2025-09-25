110d3606395fa996287e32463b53cf1d87c5bd98
rynong99 <ryon5157@colorado.edu>
2025-09-25 14:39:39 -0600

    Physics Fix
    Fixed collision behaviors and physics to respond properly to the tilesets (2 hr)

--- mob.gd
+++ mob.gd
@@ -4,7 +4,9 @@ func _ready():
 	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
 	$AnimatedSprite2D.animation = mob_types.pick_random()
 	$AnimatedSprite2D.play()
-	
+func _physics_process(delta: float) -> void:
+	move_and_collide(linear_velocity*delta)
+	linear_damp = 2
 func _process(delta):
 	if (position.x < 0) or (position.x > (1920)):
 		print("Scored")
@@ -14,8 +16,6 @@ func _process(delta):
 		print("Scored")
 		AudioManager.playScore()
 		queue_free()
-	if linear_velocity > Vector2(100,0) or linear_velocity > Vector2(0,100):
-		linear_damp = 2
-		self.set_collision_mask_value(2,false)
+	#self.set_collision_mask_value(2,false)
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
--- player.gd
+++ player.gd
@@ -8,21 +8,26 @@ var screen_size
 func ready():
 	screen_size = get_viewport_rect().size
 	hide()
-func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-		$CollisionShape2D.rotation_degrees = 90
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-		$CollisionShape2D.rotation_degrees = 270
+#func _physics_process(delta: float) -> void:
+	#move_and_slide()
+	
+func _physics_process(delta: float) -> void:
+	#var velocity = Vector2.ZERO # The player's movement vector.
+	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+	'''if Input.is_action_pressed("move_right"):
+		velocity.x += speed
+	elif Input.is_action_pressed("move_left"):
+		velocity.x -= speed
+	else:
+		velocity.x = 0
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-		$CollisionShape2D.rotation_degrees = 180
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-		$CollisionShape2D.rotation_degrees = 0
-
+		velocity.y += speed
+	elif Input.is_action_pressed("move_up"):
+		velocity.y -= speed		
+	else:
+		velocity.y = 0 '''
+	velocity = direction * 80
+	max_slides = 0
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
@@ -38,26 +43,24 @@ func _process(delta):
 	elif velocity.y > 0:
 		$AnimatedSprite2D.animation = "down"
 		
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, get_viewport_rect().size)
-	
-
-func _on_body_entered(body: Node2D) -> void:
-	#hide() # Player disappears after being hit.
-	move_and_slide()
-	
+	#position += velocity * delta
+	#position = position.clamp(Vector2.ZERO, get_viewport_rect().size)
+	var collision = move_and_collide(velocity * delta)
+	if collision and collision.get_collider() is Mob:
+		var collider = collision.get_collider()
+		#$CollisionShape2D.disabled = true
+		print("Hit Mob")
+		var push_dir = collision.get_normal()*-1
+		collider.apply_central_impulse(push_dir*200)
 func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
-
-
 func _on_player_hit() -> void:
 	pass # Replace with function body.
 
 func triggerPowerUp():
 	powerUp.emit()
-
 
 func _on_coin_collector_coin_collected() -> void:
 	powerUp.emit()
